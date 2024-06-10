ea124d0eb33a04157e9bd5b0220e19df38cc41b1
Astra <abhaypratapsinghwd@gmail.com>
2024-06-10 13:22:37 +0530

    Form Validation Login

--- client/src/components/Username.js
+++ client/src/components/Username.js
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import { Toaster, toast } from "react-hot-toast";
 import { Link } from "react-router-dom";
 import avatar from "../assets/profile.png";
 import styles from "../styles/Username.module.css";
 
 const Username = () => {
+  const [formData, setFormData] = useState({ username: "" });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (formData.username === "") {
+      toast.error("Username is required");
+    } else if (formData.username.includes(" ")) {
+      toast.error("Invalid username");
+    } else {
+      console.log("Form submitted:", formData);
+      toast.success("Form submitted successfully!");
+    }
+  };
+
   return (
     <div className="container mx-auto flex items-center justify-center h-screen">
+      <Toaster />
       <div className={styles.glass}>
         <div className="flex flex-col items-center">
           <h4 className="text-5xl font-bold">Hello Again!</h4>
@@ -13,7 +34,7 @@ const Username = () => {
             Explore More by connecting with us.
           </span>
         </div>
-        <form className="py-1">
+        <form className="py-1" onSubmit={handleSubmit}>
           <div className="flex justify-center py-2">
             <img src={avatar} className={styles.profile_img} alt="avatar" />
           </div>
@@ -23,6 +44,9 @@ const Username = () => {
               className={styles.textBox}
               type="text"
               placeholder="Username"
+              name="username"
+              value={formData.username}
+              onChange={handleChange}
             />
             <button className={styles.btn} type="submit">
               Let's Go
