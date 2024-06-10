43f8d1b8c538ed06589a759a142b7291fa24ae0e
Astra <abhaypratapsinghwd@gmail.com>
2024-06-10 11:35:17 +0530

    Added the Login UI

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,44 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import PageNotFound from "./components/PageNotFound";
+import Password from "./components/Password";
+import Profile from "./components/Profile";
+import Recovery from "./components/Recovery";
+import Register from "./components/Register";
+import Reset from "./components/Reset";
+import Username from "./components/Username";
+
 function App() {
-  return (
-    <div className="text-3xl font-mono">
-      <h1>MERN Authentication</h1>
-    </div>
-  );
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Username></Username>,
+    },
+    {
+      path: "/register",
+      element: <Register></Register>,
+    },
+    {
+      path: "/recovery",
+      element: <Recovery></Recovery>,
+    },
+    {
+      path: "/profile",
+      element: <Profile></Profile>,
+    },
+    {
+      path: "/reset",
+      element: <Reset></Reset>,
+    },
+    {
+      path: "/password",
+      element: <Password></Password>,
+    },
+    {
+      path: "*",
+      element: <PageNotFound></PageNotFound>,
+    },
+  ]);
+  return <RouterProvider router={router}></RouterProvider>;
 }
 
 export default App;
---
+++ client/src/components/Username.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import avatar from "../assets/profile.png";
+import styles from "../styles/Username.module.css";
+
+const Username = () => {
+  return (
+    <div className="container mx-auto flex items-center justify-center h-screen">
+      <div className={styles.glass}>
+        <div className="flex flex-col items-center">
+          <h4 className="text-5xl font-bold">Hello Again!</h4>
+          <span className="py-1 text-xl w-2/3 text-center text-gray-500">
+            Explore More by connecting with us.
+          </span>
+        </div>
+        <form className="py-1">
+          <div className="flex justify-center py-2">
+            <img src={avatar} className={styles.profile_img} alt="avatar" />
+          </div>
+
+          <div className="flex flex-col items-center gap-2">
+            <input
+              className={styles.textBox}
+              type="text"
+              placeholder="Username"
+            />
+            <button className={styles.btn} type="submit">
+              Let's Go
+            </button>
+          </div>
+
+          <div className="text-center py-4">
+            <span className="text-gray-500">
+              Not a Member{" "}
+              <Link className="text-red-500" to="/register">
+                Register Now
+              </Link>
+            </span>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Username;
