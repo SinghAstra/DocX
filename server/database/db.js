59b5c0e06ee13cb78f9d805504ab4ed38bec8d85
Astra <abhaypratapsinghwd@gmail.com>
2024-06-11 15:40:03 +0530

    Mongodb Connection

---
+++ server/database/db.js
@@ -0,0 +1,13 @@
+// db.js
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+  }
+};
+
+export default connectDB;
--- server/server.js
+++ server/server.js
@@ -1,11 +1,19 @@
+import cors from "cors";
 import {} from "dotenv/config";
 import express from "express";
+import morgan from "morgan";
+import connectDB from "./database/db.js";
 
 const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(morgan("tiny"));
 
 app.get("/", (req, res) => {
-  res.json({ message: "MERN Auth Server is running." });
+  res.json({ message: "Auth Server is running." });
 });
+
+connectDB();
 
 app.listen(8080, () => {
   console.log("Server connected to http://localhost:8080");
