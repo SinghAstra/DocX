fc67f19dfa8a3834d7936e768754e8f2233ebaf6
Astra <abhaypratapsinghwd@gmail.com>
2024-06-12 11:44:29 +0530

    Connected Password to Backend

--- client/src/components/Password.js
+++ client/src/components/Password.js
@@ -1,12 +1,14 @@
 import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import axios from "axios";
 import React, { useState } from "react";
 import { Toaster, toast } from "react-hot-toast";
 import { Link } from "react-router-dom";
+import profile from "../assets/avatar_2.jpeg";
 import styles from "../styles/Username.module.css";
 
 const Password = () => {
-  const [formData, setFormData] = useState({ password: "" });
+  const [formData, setFormData] = useState({ password: "ASqw!@12" });
   const [showPassword, setShowPassword] = useState(false);
 
   const handleChange = (e) => {
@@ -23,37 +25,65 @@ const Password = () => {
   const upperCaseRegex = /[A-Z]/;
   const lowerCaseRegex = /[a-z]/;
   const specialCharRegex = /[!@#$%^&*(),.?":{}|<>]/;
+  // const validateForm = () => {
+  //   const { password } = formData;
+  //   if (!lengthRegex.test(password)) {
+  //     toast.error("Password must be at least 8 characters long");
+  //     return false;
+  //   }
+  //   if (!numberRegex.test(password)) {
+  //     toast.error("Password must contain at least one number");
+  //     return false;
+  //   }
+  //   if (!upperCaseRegex.test(password)) {
+  //     toast.error("Password must contain at least one uppercase letter");
+  //     return false;
+  //   }
+  //   if (!lowerCaseRegex.test(password)) {
+  //     toast.error("Password must contain at least one lowercase letter");
+  //     return false;
+  //   }
+  //   if (!specialCharRegex.test(password)) {
+  //     toast.error("Password must contain at least one special character");
+  //     return false;
+  //   }
+
+  //   return true;
+  // };
+
   const validateForm = () => {
     const { password } = formData;
-    if (!lengthRegex.test(password)) {
-      toast.error("Password must be at least 8 characters long");
+    if (
+      !lengthRegex.test(password) ||
+      !numberRegex.test(password) ||
+      !upperCaseRegex.test(password) ||
+      !lowerCaseRegex.test(password) ||
+      !specialCharRegex.test(password)
+    ) {
+      toast.error("Incorrect Password");
       return false;
     }
-    if (!numberRegex.test(password)) {
-      toast.error("Password must contain at least one number");
-      return false;
-    }
-    if (!upperCaseRegex.test(password)) {
-      toast.error("Password must contain at least one uppercase letter");
-      return false;
-    }
-    if (!lowerCaseRegex.test(password)) {
-      toast.error("Password must contain at least one lowercase letter");
-      return false;
-    }
-    if (!specialCharRegex.test(password)) {
-      toast.error("Password must contain at least one special character");
-      return false;
-    }
-
     return true;
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
+    const { password } = formData;
     if (validateForm()) {
-      console.log("Form submitted:", formData);
-      toast.success("Form submitted successfully!");
+      try {
+        const response = await axios.post(
+          "http://localhost:5000/api/user/login",
+          {
+            username: "sharma",
+            password,
+          }
+        );
+        console.log("response is ", response);
+        toast.success(response.data.message);
+      } catch (error) {
+        console.log("error: ", error);
+        toast.error(error.response.data.message);
+      }
     }
   };
 
@@ -64,7 +94,7 @@ const Password = () => {
         <div className="flex flex-col items-center justify-between h-full">
           <div className="flex flex-col items-center">
             <h4 className="text-3xl font-bold">Enter Password!</h4>
-            <span className="pt-3 text-base text-gray-500">
+            {/* <span className="pt-3 text-base text-gray-500">
               Password must
               <br />
               be at least 8 characters long:{" "}
@@ -101,9 +131,12 @@ const Password = () => {
               ) : (
                 <span className="text-red-400">&#10006;</span>
               )}
-            </span>
+            </span> */}
           </div>
           <form className="py-1" onSubmit={handleSubmit}>
+            <div className="flex justify-center py-2">
+              <img src={profile} className={styles.profile_img} alt="avatar" />
+            </div>
             <div className="flex flex-col items-center gap-2">
               <div className="relative w-full flex items-center">
                 <input
